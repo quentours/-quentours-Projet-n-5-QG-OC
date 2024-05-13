834c1ebbfab51f9588208b895c9f10df050c9121
Quentin Guihard <quentin.guihard@outlook.fr>
2024-05-14 00:11:48 +0200

    Corrections diverses

--- kasa/src/components/Gallery/index.jsx
+++ kasa/src/components/Gallery/index.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
-import data from '../../data/logements.json' // Import JSON data
+import React, { useState, useEffect } from 'react'
+import './styles.scss'
 
 function Gallery() {
-  const places = data // Assign imported JSON data to places
+  const [places, setPlaces] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('../../logements.json')
+        if (!response.ok) {
+          throw new Error('Erreur dans la récupération des données')
+        }
+        const data = await response.json()
+        setPlaces(data)
+      } catch (error) {
+        console.error('Erreur dans le fetch des données:', error)
+      }
+    }
+    fetchData()
+  }, [])
 
   return (
     <div className="gallery">
@@ -10,7 +26,9 @@ function Gallery() {
         <div
           key={place.id}
           className="card"
-          style={{ backgroundImage: `url(${place.cover})` }}
+          style={{
+            backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(10, 10, 10, 0) 42%, rgba(4, 4, 4, 0.205) 99.99%, rgba(0, 0, 0, 0.5) 100%), url(${place.cover})`,
+          }}
           aria-label={place.title}
         >
           <h3>{place.title}</h3>
--- kasa/src/components/RouterConfig/RouterConfig.jsx
+++ kasa/src/components/RouterConfig/RouterConfig.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Index from '../../pages/Home/index.jsx'
 import FicheLogement from '../../pages/Fiche_logement/index.jsx'
 import Error from '../../pages/Erreur/index.jsx'
-import Contact from '../../pages/Contact/index.jsx'
+import APropos from '../../pages/A-propos/index.jsx'
 
 function RouterConfig() {
   return (
@@ -11,7 +11,7 @@ function RouterConfig() {
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/Fiche_logement/:id" element={<FicheLogement />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/A-propos" element={<APropos />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
